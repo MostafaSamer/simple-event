de4ac4ff8dab8a5b57e9464276c48f1d5d8f33a5
root <root@mostafa-pc>
2023-09-26 14:14:30 -0700

    first commit

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+export default function SingleEvent() {
   return (
-    <h1>Home Page</h1>
+    <h1>Single Event</h1>
   )
 }
